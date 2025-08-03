7845649fd67e48bfa0c93fc1ed6e9176221382c1
光岡 高宏/MITSUOKA Takahiro <61106530+mitsuoka0423@users.noreply.github.com>
2025-08-03 03:29:55 +0000

    feat: カスタムAPIルート

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,6 +4,7 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
+import { registerApiRoute } from '@mastra/core/server';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
@@ -16,4 +17,26 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
+  server: {
+    apiRoutes: [
+      registerApiRoute("/", {
+        method: "GET",
+        handler: async (c) => {
+          // const mastra = c.get("mastra");
+          // const agents = await mastra.getAgent("my-agent");
+ 
+          return c.json({ message: "GET / Hello, world!" });
+        },
+      }),
+      registerApiRoute("/webhook", {
+        method: "POST",
+        handler: async (c) => {
+          // const mastra = c.get("mastra");
+          // const agents = await mastra.getAgent("my-agent");
+ 
+          return c.json({ message: "POST /webhook Hello, world!" });
+        },
+      }),
+    ],
+  },
 });
